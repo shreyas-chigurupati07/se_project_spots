f799735a4f0bdfb14411e46a0371c9ce8c08491b
Shreyas Chigurupati <84034817+shreyas-chigurupati07@users.noreply.github.com>
2025-10-15 23:29:11 -0700

    corrected code after first review

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initialCards = [
+const initialCards = [
   {
     name: "Golden Gate Bridge",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
@@ -33,7 +33,7 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 const profileFormElement = editProfileModal.querySelector(".modal__form");
 const nameInput = editProfileModal.querySelector("#edit-profile-name");
-const jobInput = editProfileModal.querySelector("#edit-profile-description");
+const descriptionInput = editProfileModal.querySelector("#edit-profile-description");
 
 const profileNameElement = document.querySelector(".profile__name");
 const profileJobElement = document.querySelector(".profile__description");
@@ -77,20 +77,43 @@ function getCardElement(data) {
   return cardElement;
 }
 
+function handleEscape(evt) {
+  if (evt.key === 'Escape') {
+    const openModal = document.querySelector('.modal_is-opened');
+    if (openModal) {
+      closeModal(openModal);
+    }
+  }
+}
+
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
+
+  document.addEventListener('keydown', handleEscape);
+  modal.addEventListener('click', handleOverlayclick);
+
 }
 
 function closeModal(modal) {
   modal.classList.remove("modal_is-opened");
+
+  document.removeEventListener('keydown', handleEscape);
+  modal.removeEventListener('click', handleOverlayclick);
 }
 
 editProfileBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
   nameInput.value = profileNameElement.textContent;
-  jobInput.value = profileJobElement.textContent;
+  descriptionInput.value = profileJobElement.textContent;
   openModal(editProfileModal);
 });
+
+function handleOverlayclick(evt) {
+  if (evt.target === evt.currentTarget) {
+    closeModal(evt.target);
+  }
+}
+
 
 editProfileCloseBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -119,7 +142,7 @@ previewCloseBtnElement.addEventListener("click", (evt) => {
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileNameElement.textContent = nameInput.value;
-  profileJobElement.textContent = jobInput.value;
+  profileJobElement.textContent = descriptionInput.value;
 
   closeModal(editProfileModal);
 }
