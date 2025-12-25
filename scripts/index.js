7a989e1308dc86ae712569b4593a93b692fabd87
Shreyas Chigurupati <84034817+shreyas-chigurupati07@users.noreply.github.com>
2025-12-25 10:14:39 -0800

    Finished part1-stage10

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+import { enableValidation, settings, disableButton } from "./validation.js";
+
 const initialCards = [
   {
     name: "Golden Gate Bridge",
@@ -177,3 +179,5 @@ initialCards.forEach(function (item) {
   const cardElement = getCardElement(item);
   cardsList.append(cardElement);
 });
+
+enableValidation(settings);
--- scripts/validation.js
+++ scripts/validation.js
@@ -1,4 +1,4 @@
-const settings = {
+export const settings = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__button",
@@ -42,7 +42,7 @@ const toggleButtonState = (inputList, buttonEl, config) => {
   }
 };
 
-const disableButton = (buttonEl, config) => {
+export const disableButton = (buttonEl, config) => {
   buttonEl.disabled = true;
   buttonEl.classList.add(config.inactiveButtonClass);
 };
@@ -67,11 +67,9 @@ const setEventListeners = (formEl, config) => {
   });
 };
 
-const enableValidation = (config) => {
+export const enableValidation = (config) => {
   const formList = document.querySelectorAll(config.formSelector);
   formList.forEach((formEl) => {
     setEventListeners(formEl, config);
   });
 };
-
-enableValidation(settings);
